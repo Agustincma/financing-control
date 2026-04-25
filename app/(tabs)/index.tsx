4a663df8c3f58436c50a378f8ec6b25770514747
Agustincma <agustin.cardozo001@gmail.com>
2026-04-25 14:58:18 -0300

    Primer commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text variant="headlineMedium">Bienvenido 👋</Text>
+      <Button mode="contained">Continuar</Button>
+    </View>
+  );
+}
